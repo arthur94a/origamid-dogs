56135f4c4ca3748575678d2b6b8a97ddaace5679
arthur94a <arthuraraujo94@hotmail.com>
2022-01-26 18:24:06 -0300

    User Header and fix for children call

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 
-export const ProtectedRoute = (children) => {
+export const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
 
   return login ? children : <Navigate to="/login" />;
