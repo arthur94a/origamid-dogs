86a2f74a84e0b22340aa32d1eb2f55e95dcc3530
arthur94a <arthuraraujo94@hotmail.com>
2022-01-28 13:21:35 -0300

    Feed Modal

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 export const Feed = () => {
-  return <div>Feed</div>;
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  return (
+    <div>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
 };
